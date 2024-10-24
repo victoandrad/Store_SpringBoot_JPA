7949754c6b8a76d2a1c240ec623f02d1e6242981
Victor Andrade <victor64gaming@gmail.com>
2024-10-24 15:28:07 -0300

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/com/victoandrad/course/entities/User.java
+++ src/main/java/com/victoandrad/course/entities/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -21,6 +23,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -71,6 +76,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
