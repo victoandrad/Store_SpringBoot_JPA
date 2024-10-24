7cb3bb1ed78987602c1af602a0fbb6eedb31d543
Victor Andrade <victor64gaming@gmail.com>
2024-10-24 16:04:56 -0300

    Category

--- src/main/java/com/victoandrad/course/config/TestConfig.java
+++ src/main/java/com/victoandrad/course/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.victoandrad.course.config;
 
+import com.victoandrad.course.entities.Category;
 import com.victoandrad.course.entities.Order;
 import com.victoandrad.course.entities.User;
 import com.victoandrad.course.entities.enums.OrderStatus;
+import com.victoandrad.course.repositories.CategoryRepository;
 import com.victoandrad.course.repositories.OrderRepository;
 import com.victoandrad.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
@@ -34,6 +39,11 @@ public class TestConfig implements CommandLineRunner {
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
     }
 
---
+++ src/main/java/com/victoandrad/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.victoandrad.course.repositories;
+
+import com.victoandrad.course.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
