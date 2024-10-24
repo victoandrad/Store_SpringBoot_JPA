390a06a8990716bdbab654503772c3e2de1c9742
Victor Andrade <victor64gaming@gmail.com>
2024-10-24 16:17:08 -0300

    Product

---
+++ src/main/java/com/victoandrad/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.victoandrad.course.repositories;
+
+import com.victoandrad.course.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
