93cedbb2aa6bc01f4dd9ee42d40e74515763eed0
Victor Andrade <victor64gaming@gmail.com>
2024-10-25 16:09:43 -0300

    Exception handling - update

--- src/main/java/com/victoandrad/course/services/UserService.java
+++ src/main/java/com/victoandrad/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.victoandrad.course.entities.User;
 import com.victoandrad.course.repositories.UserRepository;
 import com.victoandrad.course.services.exceptions.DatabaseException;
 import com.victoandrad.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -46,9 +47,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
