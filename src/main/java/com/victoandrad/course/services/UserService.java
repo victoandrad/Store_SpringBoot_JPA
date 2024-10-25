b128bba1b51753a6e7a4ab3dcc2dcf0a329c16a9
Victor Andrade <victor64gaming@gmail.com>
2024-10-25 16:05:34 -0300

    Exception handling - delete

--- src/main/java/com/victoandrad/course/services/UserService.java
+++ src/main/java/com/victoandrad/course/services/UserService.java
@@ -2,8 +2,11 @@ package com.victoandrad.course.services;
 
 import com.victoandrad.course.entities.User;
 import com.victoandrad.course.repositories.UserRepository;
+import com.victoandrad.course.services.exceptions.DatabaseException;
 import com.victoandrad.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -33,7 +36,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch(EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
